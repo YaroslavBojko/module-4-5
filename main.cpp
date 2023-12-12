7ff5f066e1b316a2e72d524f750bb99e3889fcbf
Yaroslav <yaroslav.bojko.82@bk.ru>
2023-12-12 15:41:05 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int number1;
+    std::cout << "Enter the first number: ";
+    std::cin >> number1;
+
+    int number2;
+    std::cout << "Enter the first number: ";
+    std::cin >> number2;
+
+    std::cout << "-----Checking-----" << std::endl;
+
+    if(number1 % number2 == 0)
+        std::cout << "Yes, " << number1 << " is divisible by " << number2 << " without a remainder!" << std::endl;
+    else
+        std::cout << "No, " << number1 << " is not divisible by " << number2 << " without a remainder!" << std::endl;
 }
